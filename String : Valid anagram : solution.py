3fdc7eadf24c49298f04baebc48038c8347389bf
Arun <arunnn101010@gmail.com>
2026-04-22 13:37:00 +0530

    Update String : Valid anagram : solution.py

--- String : Valid anagram : solution.py
+++ String : Valid anagram : solution.py
@@ -2,3 +2,21 @@
 # Approach - Hashing
 # Time and space complexity - O(n) & O(1)
 # Leetcode and diffculty level - 242 & easy
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+
+        vector<int> freq(26,0);
+
+        for(int i=0; i<s.size(); i++) {
+            freq[s[i] - 'a'] ++;
+            freq[t[i] - 'a'] --;
+        }
+
+        for(int i=0; i<25; i++) {
+            if(freq[i] != 0) return false;
+        }
+        return true;
+    }
+};
