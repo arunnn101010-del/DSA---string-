546408fb1f737125c01dc39553424c0dcf97a973
Arun <arunnn101010@gmail.com>
2026-04-22 15:31:44 +0530

    Update String : longest common prefix : solution.py

--- String : longest common prefix : solution.py
+++ String : longest common prefix : solution.py
@@ -1,1 +1,18 @@
+# Promblem - longest common prefix 
+# Appraoch - horizontal scanning 
+# Time and space complexity - O(n*m) & O(1)
+# leetcode and diffculty level - 14 & easy 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
 
+        for(int i=1; i<strs.size(); i++) {
+            while(strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.size()-1);
+                if(prefix == "") return "";
+            }
+        } 
+        return prefix;
+    }
+};
