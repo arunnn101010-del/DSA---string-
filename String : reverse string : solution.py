94c9e2f065b5d5cab61527206086446558c1ccb0
Arun <arunnn101010@gmail.com>
2026-04-23 13:15:11 +0530

    Add reverse string solution using two pointers
    Implemented a solution to reverse a string using two pointers.

--- String : reverse string : solution.py
+++ String : reverse string : solution.py
@@ -1,1 +1,16 @@
+# Promblem - reverse string 
+# Approach - two pointers 
+# Time and space complexity - O(n) & O(1)
+# Leetcode and diffculty level - 344 & easy 
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = 0, r = s.size()-1;
 
+        while(l < r) {
+            swap(s[l], s[r]);
+            l++;
+            r--;
+        }
+    }
+};
