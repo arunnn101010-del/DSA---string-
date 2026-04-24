5d36e3bd3ac98e4a45ba73027f84ea3bbe83824a
Arun <arunnn101010@gmail.com>
2026-04-24 22:22:44 +0530

    Update String : longest substring without repeating characters : solution.py

--- String : longest substring without repeating characters : solution.py
+++ String : longest substring without repeating characters : solution.py
@@ -1,1 +1,21 @@
+# Promblem - longest substring without repeating characters 
+# Appraoch - sliding window + hashmap
+# Time and space complexity - O(n) & O(1)
+# Leetcode and diffculty level - 3 & easy 
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> st;
+        int l = 0, ans = 0;
 
+        for(int r=0; r<s.size(); r++) {
+            while(st.count(s[r])) {
+                st.erase(s[l]);
+                l++;
+            }
+            st.insert(s[r]);
+            ans = max(ans, r-l+1);
+        } 
+        return ans;
+    }
+};
