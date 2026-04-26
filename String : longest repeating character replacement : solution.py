fdd6a8dc4dc278d72e091778d6b5c25c212bc679
Arun <arunnn101010@gmail.com>
2026-04-26 21:05:33 +0530

    Update String : longest repeating character replacement : solution.py

--- String : longest repeating character replacement : solution.py
+++ String : longest repeating character replacement : solution.py
@@ -2,3 +2,23 @@
 # Approach - sliding window + frequency count
 # Time and space complexity - 0(n) & 0(1)
 # Leetcode and diffculty level - 424 & medium 
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26,0);
+        int l = 0, maxFreq = 0, ans = 0;
+
+        for(int r=0; r<s.size(); r++) {
+            freq[s[r] - 'A']++;
+
+            maxFreq = max(maxFreq, freq[s[r] - 'A']);
+
+            while((r - l + 1) - maxFreq > k) {
+                freq[s[l] - 'A']--;
+                l++;
+            }
+            ans = max(ans, r-l+1);
+        } 
+        return ans;
+    }
+};
