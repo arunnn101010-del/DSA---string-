95fca81c4143870a25b83b1d3ae9a0ba54fd4f15
Arun <arunnn101010@gmail.com>
2026-04-26 21:05:14 +0530

    Update String : longest repeating character replacement : solution.py

--- String : longest repeating character replacement : solution.py
+++ String : longest repeating character replacement : solution.py
@@ -1,1 +1,4 @@
-
+# Promblem - longest repeating character replacement 
+# Approach - sliding window + frequency count
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffculty level - 424 & medium 
