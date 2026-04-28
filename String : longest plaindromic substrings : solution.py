a1ae032a121d542468ddf494328bb481b56f5bad
Arun <arunnn101010@gmail.com>
2026-04-28 22:44:16 +0530

    Update String : longest plaindromic substrings : solution.py

--- String : longest plaindromic substrings : solution.py
+++ String : longest plaindromic substrings : solution.py
@@ -1,1 +1,30 @@
+# Promblem - Longest palindromic substrings
+# Approach - expand around center
+# Time and space complexity - 0(n^2) & 0(1)
+# Leetcode and diffculty level - 5 & medium 
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0, maxLen = 0;
 
+        for(int i=0; i<s.size(); i++) {
+            int len1 = expand(s,i,i);
+            int len2 = expand(s,i,i+1);
+
+            int len = max(len1,len2);
+
+            if(len > maxLen) {
+                maxLen = len;
+                start = i - (len-1)/2;
+            }
+        }
+        return s.substr(start,maxLen);
+    }
+    int expand(string &s, int l, int r) {
+        while(l>=0 && r < s.size() && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        return r-l-1;
+    }
+};
