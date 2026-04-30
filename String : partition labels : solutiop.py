364030606b73577ef2e537c95fb44a40b4b8c632
Arun <arunnn101010@gmail.com>
2026-04-30 22:29:04 +0530

    Update String : partition labels : solutiop.py

--- String : partition labels : solutiop.py
+++ String : partition labels : solutiop.py
@@ -1,1 +1,26 @@
+# Promblems - Partition labels 
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and dufficulty level - 763 & medium
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> last(26);
 
+        for(int i=0; i<s.size(); i++) {
+            last[s[i] - 'a'] = i;
+        }
+
+        vector<int> res;
+        int start = 0, end = 0;
+
+        for(int i=0; i<s.size(); i++) {
+            end = max(end,last[s[i] - 'a']);
+
+            if(i == end) {
+                res.push_back(end - start + 1);
+                start = i+1;
+            }
+        }
+        return res;
+    }
+};
