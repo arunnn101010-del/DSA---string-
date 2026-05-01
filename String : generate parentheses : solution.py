8eec83bf4d0fbd34c8bb2aa7a4bc8ae99be9c216
Arun <arunnn101010@gmail.com>
2026-05-01 23:46:50 +0530

    Add backtracking solution for generating parentheses
    Implemented backtracking solution to generate all combinations of well-formed parentheses.

--- String : generate parentheses : solution.py
+++ String : generate parentheses : solution.py
@@ -1,1 +1,27 @@
+# Promblem - generate parentheses 
+# Approach - backtracking 
+# Time and space complexity - 0(n^2) & 0(1)
+# leetcode & diffculty level - 22 & medium
+class Solution {
+public:
+    void solve(int open, int close, int n, string curr, vector<string>& res) {
 
+        if(curr.size() == 2 * n) {
+        res.push_back(curr);
+        return;
+        }
+
+        if(open < n) {
+        solve(open+1, close, n, curr + '(', res);
+        }
+
+        if(close < open) {
+        solve(open, close+1, n, curr + ')', res);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        solve(0, 0, n, "", res);
+        return res;
+    }
+};
