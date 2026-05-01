0f15649e49bf511ce566c0820df4f05ba2ab4389
Arun <arunnn101010@gmail.com>
2026-05-01 22:27:35 +0530

    Update String : String to interger : solution.py

--- String : String to interger : solution.py
+++ String : String to interger : solution.py
@@ -1,1 +1,31 @@
+# Promblem - strinf to interger 
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffculty level - 8 & medium 
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int sign = 1;
+        int n = s.size();
+        long result = 0;
 
+        while(i < n && s[i] == ' ') {
+            i++;
+        }
+
+        if(i < n && (s[i] == '+' || s[i] == '-')) {
+            if(s[i] == '-') sign = -1;
+            i++;
+        }
+
+        while(i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+            result = result * 10 + digit;
+
+            if(result * sign > INT_MAX) return INT_MAX;
+            if(result * sign < INT_MIN) return INT_MIN;
+            i++;
+        }
+        return result*sign;
+    }
+};
