e5a232e328e7e07b95bd1c9b975b533dbe010e9d
Arun <arunnn101010@gmail.com>
2026-05-11 16:10:53 +0530

    Add asteroid collision solution using stack
    Implemented asteroid collision handling using a stack to determine the final state of asteroids after collisions.

--- Stack : asteroid collision : solution.py
+++ Stack : asteroid collision : solution.py
@@ -1,1 +1,45 @@
+# Promblem - asteroid collision  
+# Approach - stack 
+# Time and space complexity - 0(n) & 0(n) 
+# Leetcode and diffculty level - 735 & medium 
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        
+        stack<int> st;
 
+        for(int a : asteroids) {
+
+            bool destroyed = false;
+
+            while(!st.empty() && st.top() > 0 && a < 0) {
+
+                if(abs(st.top()) < abs(a)) {
+                    st.pop();
+                }
+                else if(abs(st.top()) == abs(a)) {
+                    st.pop();
+                    destroyed = true;
+                    break;
+                }
+                else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if(!destroyed) {
+                st.push(a);
+            }
+        }
+
+        vector<int> ans(st.size());
+
+        for(int i = st.size() - 1; i>=0; i--) {
+
+            ans[i] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
